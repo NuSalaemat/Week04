137268d15f8cbc95419f21a2ba723c26761b4d86
“NuSalaemat” <ceo.penu@gmail.com>
2017-10-24 22:48:52 +0700

    15

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,10 +6,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            .   int i;
-            Console.WriteLine("Value\tSquared\tCubed");
-            for (i = 1; i < 10; i++)
-                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+            Console.WriteLine("{0:#.###}.", 1234.56789);
 
         }
     }
