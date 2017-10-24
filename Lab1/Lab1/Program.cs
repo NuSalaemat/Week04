94900d9f155f45b8ca6533af91e025b7662f63a6
“NuSalaemat” <ceo.penu@gmail.com>
2017-10-24 22:40:51 +0700

    05

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,7 +6,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here comes a slash \\");
+            Console.WriteLine("|{0, 10}|", 999);
 
         }
     }
