966832892e41ed25620de9c901a5cd074048d0e0
“NuSalaemat” <ceo.penu@gmail.com>
2017-10-24 22:41:39 +0700

    06

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -6,7 +6,7 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("|{0, 10}|", 999);
+            Console.WriteLine("|{0,-10}|", 000);
 
         }
     }
